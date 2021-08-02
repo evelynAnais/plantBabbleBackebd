cc124e30aebeab47c016b84a8c798affe2368df3
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-02 11:00:50 -0600

    intalled dotenv

---
+++ knexfile.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const { DATABASE_URL } = process.env;
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: DATABASE_URL,
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: DATABASE_URL,
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
