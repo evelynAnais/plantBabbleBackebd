f4d406077c393407781882e714e69d940f55f52e
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-02 19:05:08 -0600

    added cors and trace errors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const logger = require('./config/logger');
+
 const app = express();
+
+app.use(cors());
+app.use(logger);
+app.use(express.json());
 
 module.exports = app;
