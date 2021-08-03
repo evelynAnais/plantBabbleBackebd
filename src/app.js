e3da5ed2f04641def9fbebaf420326f47f730e55
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-02 19:11:07 -0600

    app imports and set up

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const logger = require('./config/logger');
 
+const notFound = require('./errors/notFound');
+const errorHandler = require('./errors/errorHandler');
+
+const usersRouter = require('./users/users.router');
+const postsRouter = require('./posts/posts.router');
+
 const app = express();
 
 app.use(cors());
 app.use(logger);
 app.use(express.json());
+
+app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
